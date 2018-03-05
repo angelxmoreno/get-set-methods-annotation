5e6bfa79b2a6ed5981b4ad076b93c0e5c046c83d
Angel S. Moreno <angelxmoreno@gmail.com>
2018-03-05 11:09:21 -0500

    Added single test for testing code coverage engines

--- tests/AnalyserSpec.php
+++ tests/AnalyserSpec.php
@@ -1,1 +1,13 @@
 <?php
+
+use Axm\GetSetAnnotations\Analyser;
+use Symfony\Component\ClassLoader\ClassMapGenerator;
+
+describe(Analyser::class, function () {
+    it('creates a class/file mapper', function(){
+        expect(ClassMapGenerator::class)
+            ->toReceive('::createMap');
+
+        Analyser::path(__DIR__);
+    });
+});
